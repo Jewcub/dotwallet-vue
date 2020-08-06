dd8e60e6c8b5d8aa836dc2430cf5d0e1b9d01849
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2020-08-06 17:57:26 +0800

    variable from boilerplate

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: path.resolve(`${__dirname}./../src/index.ts`),
   output: {
     path: path.resolve(`${__dirname}./../dist`),
-    filename: 'v-breadcrumps.js',
+    filename: 'dotwallet-vue.js',
   },
   module: {
     rules: [
